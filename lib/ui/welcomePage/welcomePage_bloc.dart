95c5aa26006b1a04b937111d7d3a384b74719229
Mito77 <midosayed055@gmail.com>
2023-10-19 19:18:20 +0300

    mito 19-10-2023

---
+++ lib/ui/welcomePage/welcomePage_bloc.dart
@@ -0,0 +1,18 @@
+
+import 'package:bashtil/bases/button_bloc.dart';
+
+class HomeBloc extends ButtonBloc {
+
+
+
+  @override
+  void init() {
+    super.init();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
